099c421f3f8f718200abdd40f51acf8fdcc87d53
AlexWood110 <wu-yewu@qq.com>
2016-09-24 11:30:34 +0800

    可运行版本

--- McsasApp/src/main/java/mcsas/sgcc/com/mcsas/MainActivity.java
+++ McsasApp/src/main/java/mcsas/sgcc/com/mcsas/MainActivity.java
@@ -1,5 +1,6 @@
 package mcsas.sgcc.com.mcsas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -91,11 +93,12 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
-
+            Intent intent = new Intent(this, MenuMainActivity.class);
+            startActivity(intent);
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+       DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+       drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 }
